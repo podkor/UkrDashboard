aad520d993f00ba5541c88a926b2394a35c2d0fe
serhii.pidkorytov <serhii.pidkorytov@suntechinnovation.com>
2023-03-23 13:29:35 +0200

    Initial UkrDashboard commit

---
+++ src/main/java/com/podkor/ukrdashboard/UkrDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.podkor.ukrdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkrDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkrDashboardApplication.class, args);
+    }
+
+}
